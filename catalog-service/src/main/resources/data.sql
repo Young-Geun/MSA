97b8b9bb0cf72d0a55e5bee860c4c5b71d38de29
choi <younggeunn@naver.com>
2023-10-15 14:04:07 +0900

    Catalogs Microservice - 기능 구현

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into catalog (product_id, product_name, stock, unit_price) values ('CATALOG-001', 'MacBook', 100, 3000);
+insert into catalog (product_id, product_name, stock, unit_price) values ('CATALOG-002', 'iPhone', 100, 1000);
+insert into catalog (product_id, product_name, stock, unit_price) values ('CATALOG-003', 'iPad', 100, 2000);
