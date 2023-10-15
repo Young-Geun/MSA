2033197379904ef9b3070328d6eb202f9672de70
choi <younggeunn@naver.com>
2023-10-15 15:33:22 +0900

    Orders Microservice - 기능 구현

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,52 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.service.OrderService;
+import com.example.orderservice.vo.RequestOrder;
+import com.example.orderservice.vo.ResponseOrder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/order-service")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/{userId}/orders")
+    public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId, @RequestBody RequestOrder order) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        OrderDto orderDto = mapper.map(order, OrderDto.class);
+        orderDto.setUserId(userId);
+        OrderDto returnDto = orderService.createOrder(orderDto);
+
+        ResponseOrder responseOrder = mapper.map(returnDto, ResponseOrder.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public ResponseEntity<Iterable<ResponseOrder>> getOrders(@PathVariable("userId") String userId) {
+        Iterable<OrderEntity> orders = orderService.getOrdersByUserId(userId);
+
+        List<ResponseOrder> result = new ArrayList<>();
+        orders.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseOrder.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+}
