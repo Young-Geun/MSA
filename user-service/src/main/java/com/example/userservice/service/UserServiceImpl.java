13ade9efd3a76bb11e8b3e5b994abe2703927e81
choi <younggeunn@naver.com>
2023-10-22 21:18:56 +0900

    FeignClient 예외 처리 및 로그출력

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,24 +5,23 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -67,8 +66,13 @@ public class UserServiceImpl implements UserService {
 //        List<ResponseOrder> orders = ordersResponse.getBody();
 
         // Using FeignClient
-        List<ResponseOrder> orders = orderServiceClient.getOrders(userId);
-        userDto.setOrders(orders);
+        List<ResponseOrder> orders = null;
+        try {
+            orders = orderServiceClient.getOrders(userId);
+            userDto.setOrders(orders);
+        } catch (FeignException e) {
+            log.error("orderServiceClient.getOrders Err", e);
+        }
 
         return userDto;
     }
