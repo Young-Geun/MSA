4d3e6f0adfac291cb7880b08e1fa0eb5a2306ee2
choi <younggeunn@naver.com>
2023-10-22 17:37:27 +0900

    RestTemplate 사용
    * @LoadBalanced
    @LoadBalanced 어노테이션을 사용함으로써 order-service를 호출할 때,
    IP와 Port체계가 아닌 어플리케이션명을 사용하여 아래와 같이 변경하여 호출할 수 있다.
    - 변경 전 : http://127.0.0.1:8000/order-service/%s/orders
    - 변경 후 : http://ORDER-SERVICE-0/order-service/%s/orders

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,11 +7,16 @@ import com.example.userservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +26,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final Environment env;
+    private final RestTemplate restTemplate;
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
@@ -46,7 +53,15 @@ public class UserServiceImpl implements UserService {
         }
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
-        List<ResponseOrder> orders = new ArrayList<>();
+
+        String orderUrl = String.format(env.getProperty("order-service.url"), userId);
+        ResponseEntity<List<ResponseOrder>> ordersResponse = restTemplate.exchange(
+                orderUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<ResponseOrder>>() {
+        });
+        List<ResponseOrder> orders = ordersResponse.getBody();
         userDto.setOrders(orders);
 
         return userDto;
